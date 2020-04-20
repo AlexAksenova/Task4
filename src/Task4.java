5466ee9836368d77c909264ad51f559c8bbeea1c
AlexAksenova <saneshka916@gmail.com>
2020-04-20 21:34:50 +0300

    Initial commit

---
+++ src/Task4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Task4 {
+        public static void main (String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Input binary number: ");
+            String bin = sc.nextLine();
+            int binNum = Integer.parseInt(bin, 2);
+            System.out.println(binNum);
+        }
+
+    }
+
+
